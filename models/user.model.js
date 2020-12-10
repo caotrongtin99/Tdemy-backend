6ea414240fc59220ec965609d1c352faa444b351
thong.nguyen <thong.nguyen@ITC-THONGNGUYEN-CPU1801010.local>
2020-12-10 13:56:52 +0700

    update

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('user', {
+    const User = sequelize.define('Users', {
         id: {
             type: DataTypes.UUID,
             defaultValue: DataTypes.UUIDV4,
