f8031aa59737733eff91169facb283b7d8322a9a
DianaSensei <43933486+DianaSensei@users.noreply.github.com>
2020-12-12 20:42:54 +0700

    update auth mdw

--- middleware/auth.mdw.js
+++ middleware/auth.mdw.js
@@ -1,21 +1,28 @@
 const redisClient = require("../utils/redis");
 const logger = require("../utils/log");
+const response = require("../constants/response");
 
-let auth = function (req, res, next) {
+let auth_role = role => (req, res, next) => {
   const reqData = req.body;
   redisClient.hgetall(reqData.accessToken, async function (err, data) {
     // accessToken not exists
     if (err || data === null) {
       logger.info("AccessToken %s not exist in redis!", reqData.accessToken);
-      return res.status(400).json({
-        message: "Access token not found.",
-      });
+      return res.status(400).json(response({},400, "Access token not valid"));
     } else {
       logger.info("AccessToken data: %s", data);
+      if(data.role !== role){
+        logger.info("Role [%s] not have privileges!", data.role);
+        return res.status(400).json(response({},400, "You do not privileges"));
+      }
       next();
     }
   });
 };
+let auth_enroll = enroll_key => (req, res, next) =>{
+  next();
+}
 module.exports = {
-  auth,
+  auth_role,
+  auth_enroll,
 };
