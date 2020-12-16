93b5095647490042a0fb04fa31dbf96b480c0e14
DianaSensei <43933486+DianaSensei@users.noreply.github.com>
2020-12-16 14:26:11 +0700

    update get all

--- routes/search.route.js
+++ routes/search.route.js
@@ -6,5 +6,11 @@ const logger = require("../utils/log");
 const auth_role = require("../middleware/auth.mdw").auth_role;
 
 router.post("/", async function(req, res){
+    const {limit, offset} = req.query;
+    const category = req.query.category;
+    const fee = req.query.fee;
+    const enroll = req.query.enroll;
+    const update = req.query.update;
 
+    res.json(response({},0,"success"));
 })
